73194145551398fe50b078cac53ad3575995e8bb
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-02-22 20:48:43 +0000

    Python package that is structurally capable of integrating well with Airflow

---
+++ airflow-provider-tea-pot/.angreal/init.py
@@ -0,0 +1,33 @@
+import angreal
+from angreal.integrations.git import Git
+from angreal.integrations.venv import VirtualEnv
+
+import subprocess
+import os
+
+
+
+
+def init():
+    os.chdir("airflow-provider-tea-pot")
+    
+    try:
+     VirtualEnv(".venv", now=True, requirements=".[dev]").install_requirements()
+        g = Git()
+        g.init()
+        g.add('.')
+    
+        subprocess.run(
+            (
+            "pre-commit install;"
+            "pre-commit run --all-files;"
+            "pre-commit run --all-files;" 
+            ),
+            shell=True,
+        )
+    
+        g.commit("-am 'airflow-provider-tea-pot initialized via angreal'")
+    except :
+        pass
+        
+    pass
